2335b13d5fd1e774202acaf6e5d5dacfee151cf5
marcin_srarcin <marcinbielecki95@gmail.com>
2020-07-12 20:31:12 +0200

    working

--- main.py
+++ main.py
@@ -34,42 +34,15 @@ def split_sequence(seq, n_steps_in, n_steps_out):
     return np.array(X), np.array(y)
 
 
-def predict(model, i):
-    futurePrices = np.array(pd.read_csv("BTC_EUR_last_month_gdax.csv")["Price"])
-
-    testX1 = prices[len(prices) - n_per_in + i:len(prices)]
-    testX2 = futurePrices[len(futurePrices) - i:len(futurePrices)][::-1]
-
-    futurePrices = futurePrices[len(futurePrices) - n_per_out - i:len(futurePrices) - i][::-1]
-
-    if len(testX2) > 0:
-        testX = [*testX1, *scaler.fit_transform(testX2.reshape(len(testX2), 1))]
-    else:
-        testX = testX1
-
-    testX = np.reshape(testX, (1, n_per_in, n_features))
-
-    preds = model.predict(testX, batch_size=2)
-    preds = scaler.inverse_transform(preds.reshape(-1, 1))
-
-    plt.figure(figsize=(10, 5))
-    plt.plot(futurePrices, color='red', label='Real BTC-EUR Price')
-    plt.plot(preds, color='blue', label='LSTM BTC-EUR Prediction')
-    plt.title('Bitcoin Price Prediction')
-    plt.xlabel('Time')
-    plt.ylabel('Bitcoin Price')
-    plt.legend()
-    plt.grid(True, linestyle='-.')
-    plt.savefig("predict-" + str(i))
-
-
 prices = get_prices("BTC_EUR_historical_gdax.csv")
+futurePrices = prices[len(prices) - 100:len(prices)]
+prices = prices[0:len(prices)-100]
 
 # How many periods looking back to train
 n_per_in = 30
 
 # How many periods ahead to predict
-n_per_out = 10
+n_per_out = 1
 
 # Features (in this case it's 1 because there is only one feature: price)
 n_features = 1
@@ -82,18 +55,18 @@ X = X.reshape((X.shape[0], X.shape[1], n_features))
 
 # LSTM Model parameters, I chose
 batch_size = 2  # Batch size (you may try different values)
-epochs = 200  # Epoch (you may try different values)
+epochs = 150  # Epoch (you may try different values)
 loss = 'mean_squared_error'  # Since the metric is MSE/RMSE
 optimizer = 'rmsprop'  # Recommended optimizer for RNN
 activation = 'linear'  # Linear activation
 input_shape = (n_per_in, n_features)  # Input dimension
-output_dim = 10  # Output dimension
+output_dim = n_per_out  # Output dimension
 
 '''model = Sequential()
 model.add(LSTM(units=output_dim, return_sequences=True, input_shape=input_shape))
 model.add(Dense(units=32, activation=activation))
 model.add(LSTM(units=output_dim, return_sequences=False))
-model.add(Dense(units=10, activation=activation))
+model.add(Dense(units=output_dim, activation=activation))
 model.compile(optimizer=optimizer, loss=loss)
 
 model.fit(x=X,
@@ -105,5 +78,20 @@ model.fit(x=X,
 model.save('coin_predictor.h5')'''
 model = load_model('coin_predictor.h5')
 
-for i in range(0, 29):
-    predict(model, i)
+
+testX, testY = split_sequence(futurePrices, n_per_in, n_per_out)
+testX = testX.reshape((testX.shape[0], testX.shape[1], n_features))
+testY = np.concatenate(testY).ravel().tolist()
+
+preds = model.predict(testX, batch_size=2)
+preds = scaler.inverse_transform(preds.reshape(-1, 1))
+
+plt.figure(figsize=(10, 5))
+plt.plot(scaler.inverse_transform(np.reshape(testY, (-1, 1))), color='red', label='Real BTC-EUR Price')
+plt.plot(preds, color='blue', label='LSTM BTC-EUR Prediction')
+plt.title('Bitcoin Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Bitcoin Price')
+plt.legend()
+plt.grid(True, linestyle='-.')
+plt.savefig("predict")
